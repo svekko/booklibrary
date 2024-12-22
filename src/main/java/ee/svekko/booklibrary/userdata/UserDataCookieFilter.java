3efc31ca9f70c057e6c8a4a83fe7c9033225da92
Sven-Kristjan Kompus <sven.kristjan.kompus@gmail.com>
2024-12-22 14:39:18 +0200

    Rename AuthFilter to UserDataCookieFilter

---
+++ src/main/java/ee/svekko/booklibrary/userdata/UserDataCookieFilter.java
@@ -0,0 +1,25 @@
+package ee.svekko.booklibrary.userdata;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order
+@RequiredArgsConstructor
+public class UserDataCookieFilter implements Filter {
+    private final UserDataCookieService userDataCookieService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (response instanceof HttpServletResponse) {
+            userDataCookieService.setUserDataCookie((HttpServletResponse) response);
+        }
+
+        chain.doFilter(request, response);
+    }
+}
